501a372173774650f6795b156212c85201539caa
thapanabin <thapanabincr7@outlook.com>
2019-09-13 23:03:34 -0500

    *ngIf example

--- first-angular-app/src/app/product/product.component.ts
+++ first-angular-app/src/app/product/product.component.ts
@@ -9,24 +9,7 @@ export class ProductComponent implements OnInit {
   products : any =[];
   constructor() { 
     this.products = [
-      {
-        "id": 111,
-        "name": "Iphone",
-        "brand": "Apple",
-        "price":1100
-      },
-      {
-        "id": 222,
-        "name": "Samsung Note 10",
-        "brand": "Samsung",
-        "price":1200
-      },
-      {
-        "id": 333,
-        "name": "Google Pixel",
-        "brand": "Google",
-        "price":300
-      }
+      
     ]
   }
 
