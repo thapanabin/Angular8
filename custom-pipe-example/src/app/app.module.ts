b626e9d92a277bbbd7828849affc1457056dc77b
thapanabin <thapanabincr7@outlook.com>
2019-09-23 18:23:29 -0500

    created new project

---
+++ custom-pipe-example/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayersComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
